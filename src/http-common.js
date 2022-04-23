f944aaecf95ddd6313e64c67c087d0ecbe5a92df
Drema <dadonovdema@gmail.com>
2022-04-23 18:22:42 +0300

    baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -12,12 +12,12 @@
 const token = localStorage.getItem('token')
 import axios from 'axios/dist/axios'
 export const HTTP = axios.create({
-  baseUrl: 'https://dev0api.transporter.geekchain.dev/',
+  baseURL: 'https://dev0api.transporter.geekchain.dev/',
   headers: {
     'Authorization': `Bearer ${token}`
 ,
   }
 })
 export const HTTPAUTH = axios.create({
-  baseUrl: 'https://dev0api.transporter.geekchain.dev/',
+  baseURL: 'https://dev0api.transporter.geekchain.dev/',
 })
